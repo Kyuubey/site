c7aa70ecda3c6cdc97b9d76c4a7bebbd7e2e7ee1
yuwui <me@yui.pet>
2017-09-21 16:47:15 +0200

    add logs page

--- src/main/kotlin/info/kyubey/site/Main.kt
+++ src/main/kotlin/info/kyubey/site/Main.kt
@@ -1,5 +1,6 @@
 package info.kyubey.site
 
+import com.rethinkdb.RethinkDB
 import spark.ModelAndView
 import spark.kotlin.get
 import spark.kotlin.port
@@ -13,6 +14,17 @@ class Item(val features: List<Feature>, val title: String = "Features")
 fun main(args: Array<String>) {
     port(1991)
     staticFiles.location("/public")
+
+    val r = RethinkDB.r
+
+    // TODO switch to PostgreSQL once rewrite is up
+
+    val conn = r.connection()
+            .hostname(System.getenv("KYUBEY_DB_HOST"))
+            .db(System.getenv("KYUBEY_DB_NAME"))
+            .user(System.getenv("KYUBEY_DB_USER"), System.getenv("KYUBEY_DB_PASS"))
+            .connect()
+
     get("/") {
         val model = HashMap<String, Any>()
         val items = mutableListOf<Item>()
@@ -38,6 +50,54 @@ fun main(args: Array<String>) {
 
         VelocityTemplateEngine().render(
                 ModelAndView(model, "faq.vm")
+        )
+    }
+
+    get("/logs/:channel/:timestamp") {
+        val model = HashMap<String, Any>()
+
+        // val first = request.params(":timestamp").toBigInteger() - 7200000.toBigInteger()
+        var query: List<Any>? = null
+
+        try {
+            query = r
+                    .table("Logs")
+                    .orderBy().optArg("index", r.desc("timestamp"))
+                    .filter { log ->
+                        log
+                                .g("channel")
+                                .g("id")
+                                .eq(request.params(":channel"))
+                                // FIXME meme
+                                /*.and(
+                                        log
+                                                .g("timestamp")
+                                                .lt(request.params(":timestamp").toBigInteger())
+                                )
+                                .and(
+                                        log
+                                                .g("timestamp")
+                                                .gt(first)
+                                )*/
+                    }
+                    .limit(100)
+                    .coerceTo("array")
+                    .run<List<Any>>(conn)
+        } catch (e: Throwable) {
+            e.printStackTrace()
+        }
+
+        if (query != null) {
+            query = query.filter {
+                // this is safe, trust me
+                (it as Map<String, Any>)["timestamp"].toString().toBigInteger() <= request.params(":timestamp").toBigInteger()
+            }
+
+            model.put("logs", query)
+        }
+
+        VelocityTemplateEngine().render(
+                ModelAndView(model, "logs.vm")
         )
     }
 }
